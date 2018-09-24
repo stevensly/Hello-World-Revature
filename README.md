419a31af088900942883d6b545191744533d497a
stevensly <steven.f.sly@protonmail.com>
2018-09-24 16:26:24 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello World
+This is a hello world app created by Steven Sly. To use this: 
+
+>javac -d bin src/demo/HelloWorld.java
+>java -cp bin demo.HelloWorld YourNameHere
+
+This will compile a class file in bin, with the FQCN (fully qualified class name) of demo.HelloWorld.
---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package demo;
+
+//Fully qualified class name = demo.HelloWorld
+public class HelloWorld {
+	public static void main(String[] args) {
+		// Double forward slash - comment until end of line		
+		//System.out.println(args.length);
+		
+		/* prints Hello and first arg entry */
+		System.out.println("Hello, " + args[0]);
+	}
+}
